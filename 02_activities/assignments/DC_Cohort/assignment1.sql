5c58c7e748201be265b0cc39e1fe78570d96422d
springzhouxx <xiaoxu.zhou@mail.utoronto.ca>
2026-04-08 00:46:10 -0400

    fix the conflict.sql

--- 02_activities/assignments/DC_Cohort/assignment1.sql
+++ 02_activities/assignments/DC_Cohort/assignment1.sql
@@ -30,20 +30,15 @@ LIMIT 10;
 --WHERE
 /* 1. Write a query that returns all customer purchases of product IDs 4 and 9. 
 Limit to 25 rows of output. */
-<<<<<<< Updated upstream
 --QUERY 3
-
-
-
-
---END QUERY
-
-=======
 SELECT *
 FROM customer_purchases
 WHERE product_id IN (4, 9)
 LIMIT 25;
->>>>>>> Stashed changes
+
+
+
+--END QUERY
 
 
 /*2. Write a query that returns all customer purchases and a new calculated column 'price' (quantity * cost_to_customer_per_qty), 
@@ -52,16 +47,12 @@ filtered by customer IDs between 8 and 10 (inclusive) using either:
 	2.  one condition using BETWEEN
 Limit to 25 rows of output.
 */
-<<<<<<< Updated upstream
 --QUERY 4
-
-=======
---QUERY 3
 SELECT *, quantity*cost_to_customer_per_qty AS price
 FROM customer_purchases
 WHERE customer_id BETWEEN 8 AND 10
 LIMIT 25;
->>>>>>> Stashed changes
+
 
 
 
@@ -73,17 +64,13 @@ LIMIT 25;
 Using the product table, write a query that outputs the product_id and product_name
 columns and add a column called prod_qty_type_condensed that displays the word “unit” 
 if the product_qty_type is “unit,” and otherwise displays the word “bulk.” */
-<<<<<<< Updated upstream
 --QUERY 5
-
-=======
---QUERY 4
 SELECT product_id, product_name, 
 CASE WHEN product_qty_type = 'unit' THEN 'unit'
 		 ELSE 'bulk'
 	END as prod_qty_type_condensed
 FROM product;
->>>>>>> Stashed changes
+
 
 
 
@@ -93,11 +80,7 @@ FROM product;
 /* 2. We want to flag all of the different types of pepper products that are sold at the market. 
 add a column to the previous query called pepper_flag that outputs a 1 if the product_name 
 contains the word “pepper” (regardless of capitalization), and otherwise outputs 0. */
-<<<<<<< Updated upstream
 --QUERY 6
-
-=======
---QUERY 5
 SELECT product_id, product_name, 
 CASE WHEN product_qty_type = 'unit' THEN 'unit'
 		 ELSE 'bulk'
@@ -106,7 +89,6 @@ CASE WHEN product_qty_type = 'unit' THEN 'unit'
 		 ELSE 0
 	END as pepper_flag
 FROM product;
->>>>>>> Stashed changes
 
 
 
@@ -117,18 +99,14 @@ FROM product;
 /* 1. Write a query that INNER JOINs the vendor table to the vendor_booth_assignments table on the 
 vendor_id field they both have in common, and sorts the result by market_date, then vendor_name.
 Limit to 24 rows of output. */
-<<<<<<< Updated upstream
---QUERY 7
 
-=======
---QUERY 6
+--QUERY 7
 SELECT *
 FROM vendor as v
 INNER JOIN vendor_booth_assignments as vba
 	ON v.vendor_id = vba.vendor_id
 ORDER BY market_date, vendor_name
 LIMIT 24;
->>>>>>> Stashed changes
 
 
 
@@ -141,18 +119,11 @@ LIMIT 24;
 -- AGGREGATE
 /* 1. Write a query that determines how many times each vendor has rented a booth 
 at the farmer’s market by counting the vendor booth assignments per vendor_id. */
-<<<<<<< Updated upstream
 --QUERY 8
-
-=======
---QUERY 7
 SELECT vendor_id, 
 count(vendor_id) AS vendor_rent_booth
 FROM vendor_booth_assignments
 GROUP BY vendor_id;
->>>>>>> Stashed changes
-
-
 
 --END QUERY
 
@@ -162,14 +133,10 @@ sticker to everyone who has ever spent more than $2000 at the market. Write a qu
 of customers for them to give stickers to, sorted by last name, then first name. 
 
 HINT: This query requires you to join two tables, use an aggregate function, and use the HAVING keyword. */
-<<<<<<< Updated upstream
 --QUERY 9
-=======
---QUERY 8
 SELECT c.customer_id,
 customer_first_name, customer_last_name,
 SUM(quantity*cost_to_customer_per_qty) as total_spend
->>>>>>> Stashed changes
 
 FROM customer_purchases as cp
 INNER JOIN customer as c
@@ -195,10 +162,7 @@ When inserting the new vendor, you need to appropriately align the columns to be
 -> To insert the new row use VALUES, specifying the value you want for each column:
 VALUES(col1,col2,col3,col4,col5) 
 */
-<<<<<<< Updated upstream
 --QUERY 10
-=======
---QUERY 9
 DROP TABLE IF EXISTS temp.new_vendor;
 
 CREATE TABLE temp.new_vendor AS 
@@ -206,7 +170,6 @@ SELECT * FROM vendor;
 
 INSERT INTO temp.new_vendor (vendor_id, vendor_name, vendor_type, vendor_owner_first_name, vendor_owner_last_name)
 VALUES (10, 'Thomass Superfood Store', 'a Fresh Focused store', 'Thomas', 'Rosenthal');
->>>>>>> Stashed changes
 
 
 
